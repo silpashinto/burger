c7c48d39f9fd44e871d46744e811e11206b31d04
Silpa <silpashinto@gmail.com>
2019-02-24 22:30:41 -0500

    all files

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name,devoured) VALUES ('Classic Cheeseburger',1);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Bleu Cheese Burger',1);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Texas Burger',0);
+INSERT INTO burgers (burger_name,devoured) VALUES ('Counter Burger',0);
+
---
+++ public/assets/js/eatdaburger.js
@@ -0,0 +1,55 @@
+// Make sure we wait to attach our handlers until the DOM is fully loaded.
+$(function () {
+
+  $(".create-form").on("submit", function (event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+   
+    var burgerName = $("#burger_name").val().trim();
+    if (burgerName) {
+
+      var newBurger = {
+        burger_name: burgerName,
+        devoured: 0
+      };
+      // Send the POST request.
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function () {
+          console.log("created new burger");
+          // Reload the page to get the updated list
+          location.reload();
+        }
+      );
+
+    }else{
+      alert('Not a valid entry');
+    }
+    
+  });
+
+  // $(".update-form").on("submit", function(event) {
+  $("#devour_It").on("click", function (event) {
+    // Make sure to preventDefault on a submit event.
+    event.preventDefault();
+
+    var updatedBurgers = {
+      devoured: 1
+    };
+
+    var id = $(this).data("id");
+    // Send the POST request.
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: updatedBurgers
+    }).then(
+      function () {
+        console.log("updated burger");
+        // Reload the page to get the updated list
+        location.assign("/");
+      }
+    );
+  });
+});
